dbfcb9fe316a31862bc0603918fddc7f058fde31
es10130813 <espinkheart@gmail.com>
2024-01-13 14:23:09 +0900

    init: initial commit

---
+++ src/components/ReactCalendar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
+function ReactCalendar() {
+    const [value, onChange] = useState(new Date());
+
+    const dates = {
+        '2024-01-10': 10,
+        '2024-01-15': 7,
+        '2024-01-20': 3,
+    };
+
+    const tileContent = ({ date, view }) => {
+        const dateString = date.toISOString().split('T')[0];
+
+        const value = dates[dateString];
+        let backgroundColor = '#fff'; 
+        if (value > 5) {
+            backgroundColor = '#ff9999'; 
+        } else if (value) {
+            backgroundColor = '#99ff99'; 
+        }
+
+        return (
+            <div style={{ height: '100%', backgroundColor }}>
+                {/* 여기에 추가 컨텐츠를 넣을 수 있습니다. */}
+            </div>
+        );
+    };
+
+    return (
+      <div>
+        <Calendar
+          onChange={onChange}
+          value={value}
+          tileContent={tileContent}
+        />
+      </div>
+    );
+}
+
+export default ReactCalendar;
